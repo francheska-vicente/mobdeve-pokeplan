c52a1faad256d39164c19b22b039253db854c679
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-04 23:01:51 +0800

    Update TasklistActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TasklistActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TasklistActivity.java
@@ -1,14 +1,57 @@
 package com.mobdeve.s11.pokeplan;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class TasklistActivity extends AppCompatActivity {
 
+    private ArrayList<Task> ongoingList;
+    private RecyclerView rvOngoing;
+
+    private ArrayList<Task> completedList;
+    private RecyclerView rvCompleted;
+
+    private TaskAdapter taskAdapter;
+    private FloatingActionButton fabAdd;
+
+    private ActivityResultLauncher addActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                Intent intent = result.getData();
+
+                /* TODO: add intent */
+            }
+        }
+    );
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasklist);
+
+        initComponents();
+        initFabAdd();
+    }
+
+    private void initComponents () {
+
+    }
+
+    private void initFabAdd () {
+
     }
 }
