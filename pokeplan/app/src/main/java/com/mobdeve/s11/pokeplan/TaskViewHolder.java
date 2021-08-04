64a7fece782bdd12a340b045bd733abfb204ac9f
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-04 23:01:59 +0800

    Create TaskViewHolder.java

---
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskViewHolder.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.pokeplan;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TaskViewHolder extends RecyclerView.ViewHolder {
+    public TaskViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+    }
+}
