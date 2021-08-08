4659058bc7ac5407c8f92c22ea3bed5c2d4598c0
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-08 23:45:34 +0800

    Update TaskAdapter.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskAdapter.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskAdapter.java
@@ -16,8 +16,11 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
     private ArrayList<Task> tasks;
 
     public static final String KEY_TASKNAME = "KEY_TASKNAME";
-    public static final String KEY_CATEGORY = "KEY_CATEGORY";
     public static final String KEY_DEADLINE = "KEY_DEADLINE";
+    public static final String KEY_CATEGORY = "KEY_CATEGORY";
+    public static final String KEY_PRIORITY = "KEY_PRIORITY";
+    public static final String KEY_START_DATE = "KEY_START_DATE";
+    public static final String KEY_NOTES = "KEY_NOTES";
 
 
     public TaskAdapter(ArrayList<Task> tasks) {
@@ -38,7 +41,10 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
                 Intent i = new Intent(view.getContext(), TaskDetailsActivity.class);
                 i.putExtra(KEY_TASKNAME, tasks.get(vh.getBindingAdapterPosition()).getTaskName());
                 i.putExtra(KEY_CATEGORY, tasks.get(vh.getBindingAdapterPosition()).getCategory());
+                i.putExtra(KEY_PRIORITY, tasks.get(vh.getBindingAdapterPosition()).getPriority());
                 i.putExtra(KEY_DEADLINE, tasks.get(vh.getBindingAdapterPosition()).getEndDate().toString());
+                i.putExtra(KEY_START_DATE, tasks.get(vh.getBindingAdapterPosition()).getStartDate().toString());
+                i.putExtra(KEY_NOTES, tasks.get(vh.getBindingAdapterPosition()).getDescription());
                 view.getContext().startActivity(i);
             }
         });
