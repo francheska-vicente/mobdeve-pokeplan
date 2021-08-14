e323f0090efbe0cbff0246ff8f8c1e9306f1fd90
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-14 20:07:52 +0800

    added bottom nav, changed activities to fragments

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
@@ -1,35 +1,27 @@
 package com.mobdeve.s11.pokeplan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button btnfocus;
-    private Button btntask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
-        btnfocus = findViewById(R.id.btn_focus);
-        btnfocus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), FocusTimerActivity.class);
-                view.getContext().startActivity(i);
-            }
-        });
-
-        btntask = findViewById(R.id.btn_tasklist);
-        btntask.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), TasklistActivity.class);
-                view.getContext().startActivity(i);
-            }
-        });
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
---
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/SettingsFragment.java
@@ -0,0 +1,31 @@
+package com.mobdeve.s11.pokeplan;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+    }
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+}
