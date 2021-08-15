0be57dbd1d1ed7d3816905d05149c8b5548b957f
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-15 19:16:32 +0800

    added user class

---
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserPokemon.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.pokeplan;
+
+public class UserPokemon {
+    private Pokemon details;
+    private int level;
+    private int fedcandy;
+
+    public UserPokemon(Pokemon details) {
+        this.details = details;
+        this.level = 1;
+    }
+
+
+}
