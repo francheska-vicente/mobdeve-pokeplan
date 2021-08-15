bb9a9ff8b67bff65e542b7675922f8e666c71908
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-15 18:47:24 +0800

    added priority to task list

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskDataHelper.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskDataHelper.java
@@ -19,6 +19,22 @@ public class TaskDataHelper {
 
         data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
         data.add(new Task("Genshin Impact", 1, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 2, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 4, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 5, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 3, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 1, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 2, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+        data.add(new Task("Study", 1, "School", new CustomDate(2021, 8, 20, 16, 00), new CustomDate(2021, 8, 8, 16, 00), "rawr"));
+        data.add(new Task("Genshin Impact", 1, "Leisure", new CustomDate(), new CustomDate(), "rawr"));
+
+
 
         return data;
     }
@@ -28,7 +44,7 @@ public class TaskDataHelper {
         ArrayList<Task> data = new ArrayList<Task>();
 
         data.add(new Task("Bake", 1, "Hobby", new CustomDate(), new CustomDate(), "rawr"));
-        data.add(new Task("Exercise", 1, "Health", new CustomDate(2021, 10, 22, 19, 52), new CustomDate(2021, 10, 22, 19, 52), "rawr"));
+        data.add(new Task("Exercise", 5, "Health", new CustomDate(2021, 10, 22, 19, 52), new CustomDate(2021, 10, 22, 19, 52), "rawr"));
 
         return data;
     }
