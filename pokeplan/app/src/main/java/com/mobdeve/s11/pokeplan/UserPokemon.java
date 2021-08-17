0afbb1e0c9554a2cbd7c2161117f5a8b5e4379ef
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-17 16:42:16 +0800

    Update UserPokemon.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserPokemon.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserPokemon.java
@@ -2,11 +2,13 @@ package com.mobdeve.s11.pokeplan;
 
 public class UserPokemon {
     private Pokemon details;
+    private String nickname;
     private int level;
     private int fedcandy;
 
     public UserPokemon(Pokemon details) {
         this.details = details;
+        this.nickname = details.getSpecies();
         this.level = 1;
         this.fedcandy = 0;
     }
@@ -21,5 +23,33 @@ public class UserPokemon {
 
     public void evolvePokemon() {
 
+    }
+
+    public Pokemon getPokemonDetails() {
+        return details;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getFedcandy() {
+        return fedcandy;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setFedcandy(int fedcandy) {
+        this.fedcandy = fedcandy;
     }
 }
