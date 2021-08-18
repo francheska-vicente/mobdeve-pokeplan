3cdb1a850620fb2bc0c9fb5e376cbfc40201a4e6
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-18 23:34:18 +0800

    Update HomeFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
@@ -1,13 +1,20 @@
 package com.mobdeve.s11.pokeplan;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    private ArrayList<UserPokemon> pokemonPartyList;
+    private RecyclerView rvPokemonParty;
+    private PokemonPartyAdapter ppAdapter;
 
     public HomeFragment() {
     }
@@ -25,6 +32,20 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initComponents(view);
+        return view;
+    }
+
+    private void initComponents (View view) {
+        User helper = new User();
+
+        this.pokemonPartyList = helper.getUserPokemonParty();
+        this.rvPokemonParty = view.findViewById(R.id.rv_home_party);
+        this.rvPokemonParty.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+
+        this.ppAdapter = new PokemonPartyAdapter(this.pokemonPartyList);
+        this.rvPokemonParty.setAdapter(this.ppAdapter);
+
     }
 }
