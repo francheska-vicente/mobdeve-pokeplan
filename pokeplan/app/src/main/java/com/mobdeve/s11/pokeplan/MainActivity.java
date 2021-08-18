25ba1c5051fcbdbb30696a2d3cafb530ccb14d36
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-18 23:34:22 +0800

    Update MainActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
