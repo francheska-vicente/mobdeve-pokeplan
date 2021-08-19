2f9f481abf7cc1055960a61578999081be471601
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-20 02:09:17 +0800

    Update PokedexFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/PokedexFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/PokedexFragment.java
@@ -1,13 +1,21 @@
 package com.mobdeve.s11.pokeplan;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class PokedexFragment extends Fragment {
+    private Boolean[] pokedex;
+    private RecyclerView rvPokedex;
+    private PokedexAdapter pdAdapter;
 
     public PokedexFragment() {
     }
@@ -26,6 +34,19 @@ public class PokedexFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pokedex, container, false);
+        initComponents(view);
         return view;
+    }
+
+    private void initComponents (View view) {
+        User helper = new User();
+
+        this.pokedex = helper.getUserPokedex();
+        this.rvPokedex = view.findViewById(R.id.rv_pokedex);
+        this.rvPokedex.setLayoutManager(new GridLayoutManager(getActivity(), 5));
+
+        this.pdAdapter = new PokedexAdapter(this.pokedex);
+        this.rvPokedex.setAdapter(this.pdAdapter);
+
     }
 }
