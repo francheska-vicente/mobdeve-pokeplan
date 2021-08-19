71f7a12da7b7434c1d6cfb88c45a7ac036268e4a
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-20 00:15:49 +0800

    Update PokemonDetailsActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/PokemonDetailsActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/PokemonDetailsActivity.java
@@ -1,6 +1,150 @@
 package com.mobdeve.s11.pokeplan;
 
-public class PokemonDetailsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class PokemonDetailsActivity extends AppCompatActivity {
+    private ImageButton btnback;
+    private ImageButton btnedit;
+
+    private Button btnrare;
+    private Button btnsuper;
+
+    private ImageView ivPkmnIcon;
+    private TextView tvPkmnNickname;
+    private TextView tvPkmnSpecies;
+    private TextView tvPkmnLevel;
+    private TextView tvPkmnType;
+    private TextView tvPkmnNature;
+    private TextView tvPkmnMetDate;
+    private ProgressBar pbPkmnLevel;
+
+    private Dialog editdialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_details);
+        initComponents();
+        setAllComponents();
 
 
+    }
+
+    private void initComponents() {
+        this.btnback = findViewById(R.id.ib_pkmndetails_back);
+        this.btnback.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        this.btnedit = findViewById(R.id.ib_pkmndetails_edit);
+
+        this.btnrare = findViewById(R.id.btn_pkmndetails_rarecandy);
+
+        this.ivPkmnIcon = findViewById(R.id.iv_pkmndetails_icon);
+        this.tvPkmnNickname = findViewById(R.id.tv_pkmndetails_nickname);
+        this.tvPkmnSpecies = findViewById(R.id.tv_pkmndetails_species);
+        this.tvPkmnLevel = findViewById(R.id.tv_pkmndetails_level);
+        this.tvPkmnType = findViewById(R.id.tv_pkmndetails_type);
+        this.tvPkmnNature = findViewById(R.id.tv_pkmndetails_nature);
+        this.tvPkmnMetDate = findViewById(R.id.tv_pkmndetails_metdate);
+        this.pbPkmnLevel = findViewById(R.id.pb_pkmndetails_level);
+    }
+
+    private void setAllComponents() {
+        Intent intent = getIntent();
+        String pkmnid = intent.getStringExtra(PokemonPartyAdapter.KEY_POKEMONID);
+        UserPokemon pkmn = new User().getPokemonInParty(pkmnid);
+
+
+        this.ivPkmnIcon.setImageResource(getImageId(getApplicationContext(),
+                "pkmn_"+ pkmn.getPokemonDetails().getDexNum()));
+        this.tvPkmnNickname.setText(pkmn.getNickname());
+        this.tvPkmnSpecies.setText(pkmn.getPokemonDetails().getSpecies());
+        this.tvPkmnNature.setText(pkmn.getNature());
+        this.tvPkmnMetDate.setText(pkmn.getMetDate().toString());
+        this.pbPkmnLevel.setProgress(pkmn.getPercentToNextLevel());
+
+        String level = "Level " + pkmn.getLevel();
+        this.tvPkmnLevel.setText(level);
+
+        String pkmntype = pkmn.getPokemonDetails().getType1();
+        if (!pkmn.getPokemonDetails().getType2().isEmpty())
+            pkmntype = pkmntype + "/" + pkmn.getPokemonDetails().getType2();
+        this.tvPkmnType.setText(pkmntype);
+
+        this.btnedit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                editNickname(pkmn);
+            }
+        });
+
+        this.btnrare.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                feedPokemon(pkmn);
+            }
+        });
+    }
+
+    private void editNickname(UserPokemon pkmn) {
+        editdialog = new Dialog(this);
+        editdialog.setContentView(R.layout.dialog_stringinput);
+        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.40);
+
+        editdialog.getWindow().setLayout(width, height);
+        editdialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView tvdialogtitle = editdialog.findViewById(R.id.iv_dialog_stringinput_title);
+        tvdialogtitle.setText("Name your Pokemon!");
+        EditText etdialoginput = editdialog.findViewById(R.id.et_dialog_stringinput);
+        ImageView ivdialogicon = editdialog.findViewById(R.id.iv_dialog_stringinput_icon);
+        ivdialogicon.setImageResource(getImageId(getApplicationContext(),
+                "pkmn_"+ pkmn.getPokemonDetails().getDexNum()));
+
+        Button btndialogok = editdialog.findViewById(R.id.btn_dialog_stringinput_ok);
+        btndialogok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pkmn.setNickname(etdialoginput.getText().toString());
+                tvPkmnNickname.setText(pkmn.getNickname());
+                editdialog.dismiss();
+            }
+        });
+
+        Button btndialogcancel = editdialog.findViewById(R.id.btn_dialog_stringinput_confirm);
+        btndialogcancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editdialog.dismiss();
+            }
+        });
+
+        editdialog.show();
+    }
+
+    private void feedPokemon(UserPokemon pkmn) {
+        pkmn.feedCandy();
+        this.pbPkmnLevel.setProgress(pkmn.getPercentToNextLevel());
+        String level = "Level " + pkmn.getLevel();
+        this.tvPkmnLevel.setText(level);
+    }
+
+    private int getImageId(Context context, String imageName) {
+        return context.getResources().getIdentifier("drawable/" + imageName, null, context.getPackageName());
+    }
 }
