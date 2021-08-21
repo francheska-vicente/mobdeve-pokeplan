311f59922b39d7b8d51b9aee52978dab535e8092
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-21 17:02:43 +0800

    Update UserDetails.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserDetails.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserDetails.java
@@ -3,30 +3,25 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class UserDetails {
+    private String fullName;
+    private String email;
+    private String userName;
 
+    public UserDetails(String fullName, String email, String userName) {
+        this.fullName = fullName;
+        this.email = email;
+        this.userName = userName;
+    }
 
-        private String fullName;
-        private String email;
-        private String userName;
+    public String getName () {
+        return this.fullName;
+    }
 
-        public UserDetails(String fullName, String email, String userName) {
+    public String getEmail () {
+        return this.email;
+    }
 
-
-            this.fullName = fullName;
-            this.email = email;
-            this.userName = userName;
-
-        }
-
-        public String getName () {
-            return this.fullName;
-        }
-
-        public String getEmail () {
-            return this.email;
-        }
-
-        public String getUserName () {
-            return this.userName;
-        }
+    public String getUserName () {
+        return this.userName;
+    }
 }
