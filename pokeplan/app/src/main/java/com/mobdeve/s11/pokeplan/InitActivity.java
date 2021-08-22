a7c3fe345831dfaa8fcced2dc3c2260a83bc9482
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-23 00:32:53 +0800

    Update InitActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/InitActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/InitActivity.java
@@ -1,7 +1,9 @@
 package com.mobdeve.s11.pokeplan;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,25 +13,40 @@ public class InitActivity extends AppCompatActivity {
     private Button btninitregister;
     private Button btninitlogin;
 
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_init);
+        this.sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String email = this.sp.getString(Keys.KEY_EMAIL.name(), null);
+        String password = this.sp.getString(Keys.KEY_PASSWORD.name(), null);
 
-        btninitregister = findViewById(R.id.btn_init_register);
-        btninitregister.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), RegisterActivity.class);
-                view.getContext().startActivity(i);
-            }
-        });
+        if (email != null && password != null) {
+            Intent i = new Intent(this, MainActivity.class);
+            this.startActivity(i);
+        }
+        else {
+            setContentView(R.layout.activity_init);
 
-        btninitlogin = findViewById(R.id.btn_init_login);
-        btninitlogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), LoginActivity.class);
-                view.getContext().startActivity(i);
-            }
-        });
+            btninitregister = findViewById(R.id.btn_init_register);
+            btninitregister.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent i = new Intent(view.getContext(), RegisterActivity.class);
+                    view.getContext().startActivity(i);
+                }
+            });
+
+            btninitlogin = findViewById(R.id.btn_init_login);
+            btninitlogin.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent i = new Intent(view.getContext(), LoginActivity.class);
+                    view.getContext().startActivity(i);
+                }
+            });
+        }
+
+
+
     }
 }
