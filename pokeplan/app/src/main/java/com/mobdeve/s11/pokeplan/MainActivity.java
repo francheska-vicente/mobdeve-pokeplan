57b30b17e8355e0978243907d1a7ae15f1f31228
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-22 19:18:14 +0800

    Update MainActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,16 +18,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
-        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+                BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_fragment);
+                NavigationUI.setupWithNavController(navView, navController);
+            }
+        }, 690);
     }
 
-    @Override
-    public void onRestart () {
-        super.onRestart();
-
-    }
 }
