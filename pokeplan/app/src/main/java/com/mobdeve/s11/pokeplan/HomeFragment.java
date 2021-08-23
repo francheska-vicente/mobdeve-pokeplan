1a7cea96fba1e340d1b6612ada755cfe2e313b3e
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-24 00:03:55 +0800

    Update HomeFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
@@ -1,16 +1,22 @@
 package com.mobdeve.s11.pokeplan;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -40,7 +46,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         initComponents(view);
-    initButtons(view);
+        initButtons(view);
         return view;
     }
 
