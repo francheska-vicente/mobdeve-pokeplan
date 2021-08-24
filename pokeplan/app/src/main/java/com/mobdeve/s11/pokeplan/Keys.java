e0ad6e36f4d9c64f46bf3063f33727fbf31a59ff
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-24 23:19:52 +0800

    Update Keys.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/Keys.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/Keys.java
@@ -1,6 +1,14 @@
 package com.mobdeve.s11.pokeplan;
 
+import android.widget.Switch;
+
 public enum Keys {
     KEY_EMAIL,
-    KEY_PASSWORD
+    KEY_PASSWORD,
+
+    KEY_DEEPFOCUS,
+    KEY_DIMSCREEN,
+    KEY_KEEPSCREENON,
+    KEY_NOTIFS,
+    KEY_PKMNCRIES
 }
