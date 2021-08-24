6c366e52931762356d1f12a619e9f0d790b4271f
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-24 21:06:20 +0800

    Update SettingsFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/SettingsFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/SettingsFragment.java
@@ -6,8 +6,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class SettingsFragment extends Fragment {
+
+    private Button btnEditAcc;
+    private Button btnFreqQues;
+    private Button btnAbout;
+
+    private Switch swPkmncries;
+    private Switch swNotifs;
+
+    private Switch swScreenOn;
+    private Switch swDimScreen;
+    private Switch swDeepFocus;
 
     public SettingsFragment() {
     }
@@ -26,6 +39,19 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        initComponents(view);
         return view;
+    }
+
+    private void initComponents (View view) {
+        this.btnAbout = view.findViewById(R.id.btn_settings_about);
+        this.btnEditAcc = view.findViewById(R.id.btn_settings_editacc);
+        this.btnFreqQues = view.findViewById(R.id.btn_settings_faq);
+
+        this.swDeepFocus = view.findViewById(R.id.sw_deepfocus);
+        this.swDimScreen = view.findViewById(R.id.sw_dimscreen);
+        this.swNotifs = view.findViewById(R.id.sw_notifs);
+        this.swPkmncries = view.findViewById(R.id.sw_pkmncries);
+        this.swScreenOn = view.findViewById(R.id.sw_screenon);
     }
 }
