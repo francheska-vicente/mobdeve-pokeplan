0810100dae17b86cb62589460736633f42ff9c6f
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-25 07:02:05 +0800

    Update TasklistFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TasklistFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TasklistFragment.java
@@ -1,17 +1,7 @@
- package com.mobdeve.s11.pokeplan;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+package com.mobdeve.s11.pokeplan;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +9,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class TasklistFragment extends Fragment {
+public class TaskListFragment extends Fragment {
     private ArrayList<Task> ongoingList;
     private RecyclerView rvOngoing;
     private ImageButton ibOngoingToggle;
@@ -48,14 +36,14 @@ public class TasklistFragment extends Fragment {
 
     private Spinner spinFilter;
     private ImageButton ibFilter;
+    private TextView tvFilterLabel;
     private boolean filterIsVisible;
 
-    public TasklistFragment() {
+    public TaskListFragment() {
     }
 
-    public static TasklistFragment newInstance() {
-        TasklistFragment fragment = new TasklistFragment();
-        return fragment;
+    public static TaskListFragment newInstance() {
+        return new TaskListFragment();
     }
 
     @Override
@@ -67,7 +55,7 @@ public class TasklistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tasklist, container, false);
-        initFabAdd(view);
+
         initComponents(view);
         return view;
     }
@@ -81,59 +69,48 @@ public class TasklistFragment extends Fragment {
         this.rvCompleted = view.findViewById(R.id.rv_tasklist_completed);
         this.rvCompleted.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
 
-        initLists(this.ongoingList, this.completedList);
-
-        this.ibOngoingToggle = view.findViewById(R.id.ib_tasklist_toggleongoing);
-        this.ongoingIsVisible = true;
-        this.ibOngoingToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(ongoingIsVisible) {
-                    ongoingIsVisible = false;
-                    ibOngoingToggle.setImageResource(R.drawable.arrow_down);
-                    rvOngoing.setVisibility(View.GONE);
-                }
-                else {
-                    ongoingIsVisible = true;
-                    ibOngoingToggle.setImageResource(R.drawable.arrow_up);
-                    rvOngoing.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        this.ibCompletedToggle = view.findViewById(R.id.ib_tasklist_togglecompleted);
-        this.completedIsVisible = true;
-        this.ibCompletedToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(completedIsVisible) {
-                    completedIsVisible = false;
-                    ibCompletedToggle.setImageResource(R.drawable.arrow_down);
-                    rvCompleted.setVisibility(View.GONE);
-                }
-                else {
-                    completedIsVisible = true;
-                    ibCompletedToggle.setImageResource(R.drawable.arrow_up);
-                    rvCompleted.setVisibility(View.VISIBLE);
-                }
-            }
-        });
+        this.setTaskLists(this.ongoingList, this.completedList);
 
         this.spinFilter = view.findViewById(R.id.spinner_tasklist_filter);
         this.ibFilter = view.findViewById(R.id.ib_filter);
-        initFilter(view);
+        this.tvFilterLabel = view.findViewById(R.id.tv_tasklist_filter_label);
+        this.setFilterComponents();
+
+        this.ibOngoingToggle = view.findViewById(R.id.ib_tasklist_toggleongoing);
+        this.ibCompletedToggle = view.findViewById(R.id.ib_tasklist_togglecompleted);
+        this.setButtonListeners();
+
+
+        this.fabAdd = view.findViewById(R.id.fab_tasklist_add_task);
+        this.setFabListener();
     }
 
-    private void initFilter (View view) {
-        ArrayAdapter<CharSequence> adapterFilter = ArrayAdapter.createFromResource(getContext(),
-                R.array.tasklist_filter, R.layout.spinner_item);
-        adapterFilter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        spinFilter.setAdapter(adapterFilter);
-        spinFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                filterTask(spinFilter.getSelectedItem().toString());
+    private void setButtonListeners() {
+        this.ongoingIsVisible = true;
+        this.ibOngoingToggle.setOnClickListener(v -> {
+            if(ongoingIsVisible) {
+                ongoingIsVisible = false;
+                ibOngoingToggle.setImageResource(R.drawable.arrow_down);
+                rvOngoing.setVisibility(View.GONE);
             }
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                return;
+            else {
+                ongoingIsVisible = true;
+                ibOngoingToggle.setImageResource(R.drawable.arrow_up);
+                rvOngoing.setVisibility(View.VISIBLE);
+            }
+        });
+
+        this.completedIsVisible = true;
+        this.ibCompletedToggle.setOnClickListener(v -> {
+            if(completedIsVisible) {
+                completedIsVisible = false;
+                ibCompletedToggle.setImageResource(R.drawable.arrow_down);
+                rvCompleted.setVisibility(View.GONE);
+            }
+            else {
+                completedIsVisible = true;
+                ibCompletedToggle.setImageResource(R.drawable.arrow_up);
+                rvCompleted.setVisibility(View.VISIBLE);
             }
         });
 
@@ -143,38 +120,50 @@ public class TasklistFragment extends Fragment {
             if(filterIsVisible) {
                 filterIsVisible = false;
                 spinFilter.setVisibility(View.INVISIBLE);
+                tvFilterLabel.setVisibility(View.VISIBLE);
             }
             else {
                 filterIsVisible = true;
                 spinFilter.setVisibility(View.VISIBLE);
+                tvFilterLabel.setVisibility(View.INVISIBLE);
             }
         });
     }
 
-    private void initFabAdd (View view) {
-        this.fabAdd = view.findViewById(R.id.fab_tasklist_add_task);
-        this.fabAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AddTaskActivity.class);
-                startActivity(intent);
+    private void setFilterComponents() {
+        ArrayAdapter<CharSequence> adapterFilter = ArrayAdapter.createFromResource(getContext(),
+                R.array.tasklist_filter, R.layout.spinner_item);
+        adapterFilter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinFilter.setAdapter(adapterFilter);
+        spinFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                filterTask(spinFilter.getSelectedItem().toString());
+            }
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
     }
 
-    private void initLists (ArrayList<Task> ongoingList, ArrayList<Task> completedList) {
+    private void setFabListener() {
+        this.fabAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AddTaskActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setTaskLists (ArrayList<Task> ongoingList, ArrayList<Task> completedList) {
         this.taskAdapterOngoing = new TaskAdapter(ongoingList);
         this.rvOngoing.setAdapter(this.taskAdapterOngoing);
+        this.taskAdapterOngoing.notifyDataSetChanged();
+
         this.taskAdapterCompleted = new TaskAdapter(completedList);
         this.rvCompleted.setAdapter(this.taskAdapterCompleted);
-
         this.taskAdapterCompleted.notifyDataSetChanged();
-        this.taskAdapterOngoing.notifyDataSetChanged();
     }
 
     private void filterTask (String category) {
         if (category.equalsIgnoreCase("ALL")) {
-            initLists(this.ongoingList, this.completedList);
+            setTaskLists(this.ongoingList, this.completedList);
         }
         else {
             ArrayList<Task> tempCompleted = new ArrayList<>();
@@ -191,13 +180,13 @@ public class TasklistFragment extends Fragment {
                 }
             }
 
-            initLists(tempOngoing, tempCompleted);
+            setTaskLists(tempOngoing, tempCompleted);
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        initComponents(getView());
+        this.initComponents(getView());
     }
 }
