721d0c4d797a2424b90eec02ba0a60021897d516
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-25 07:01:05 +0800

    Update HomeFragment.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/HomeFragment.java
@@ -1,22 +1,16 @@
 package com.mobdeve.s11.pokeplan;
 
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -45,27 +39,42 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         initComponents(view);
-        initButtons(view);
+
         return view;
     }
 
-    private void initComponents (View view) {
-        this.pokemonPartyList = UserSingleton.getUser().getUserPokemonParty();
+    /**
+     * Initializes the layout's components
+     * @param view the view of the fragment
+     */
+    private void initComponents(View view) {
         this.rvPokemonParty = view.findViewById(R.id.rv_home_party);
-        this.rvPokemonParty.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        setRecyclerView();
 
+        this.ibuserprofile = view.findViewById(R.id.ib_home_user);
+        this.ibpokemonpc = view.findViewById(R.id.ib_home_pc);
+        setButtonListeners();
+    }
+
+    /**
+     * Sets the adapter of the RecyclerView
+     */
+    private void setRecyclerView() {
+        this.pokemonPartyList = UserSingleton.getUser().getUserPokemonParty();
+        this.rvPokemonParty.setLayoutManager(new LinearLayoutManager(
+                getActivity(),LinearLayoutManager.VERTICAL,false));
         this.ppAdapter = new PokemonPartyAdapter(this.pokemonPartyList);
         this.rvPokemonParty.setAdapter(this.ppAdapter);
     }
 
-    private void initButtons(View view) {
-        this.ibuserprofile = view.findViewById(R.id.ib_home_user);
+    /**
+     * Sets the onClickListeners for all buttons
+     */
+    private void setButtonListeners() {
         this.ibuserprofile.setOnClickListener(v -> {
             Intent i = new Intent(v.getContext(), UserProfileActivity.class);
             v.getContext().startActivity(i);
         });
-
-        this.ibpokemonpc = view.findViewById(R.id.ib_home_pc);
         this.ibpokemonpc.setOnClickListener(v -> {
             Intent i = new Intent(v.getContext(), PokemonPCActivity.class);
             v.getContext().startActivity(i);
