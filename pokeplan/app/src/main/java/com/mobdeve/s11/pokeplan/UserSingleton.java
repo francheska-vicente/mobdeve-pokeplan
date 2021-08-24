6d981e76951e414fc5707e4ee7ab66a206de5fca
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-24 18:06:05 +0800

    Update UserSingleton.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserSingleton.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserSingleton.java
@@ -5,16 +5,11 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-<<<<<<< Updated upstream
-import com.google.api.Property;
+
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-=======
-import com.google.firebase.auth.FirebaseAuth;
->>>>>>> Stashed changes
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
