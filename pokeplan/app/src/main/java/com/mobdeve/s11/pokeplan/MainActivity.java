e964bf4700b8405e57bd45d00e2b0fa8f4008e4e
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-25 07:01:23 +0800

    Update MainActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/MainActivity.java
@@ -15,16 +15,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         UserSingleton.getUser();
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                setContentView(R.layout.activity_main);
-                BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
-                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_fragment);
-                NavigationUI.setupWithNavController(navView, navController);
-            }
+        new Handler().postDelayed(() -> {
+            setContentView(R.layout.activity_main);
+            BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+            NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_fragment);
+            NavigationUI.setupWithNavController(navView, navController);
         }, 1000);
     }
 
