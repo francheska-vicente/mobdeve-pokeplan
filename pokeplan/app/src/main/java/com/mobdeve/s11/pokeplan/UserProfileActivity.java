e2005cfb37a264fb997a3a987f761d01b1ba2fe6
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-25 09:34:05 +0800

    Update UserProfileActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserProfileActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserProfileActivity.java
@@ -1,23 +1,17 @@
 package com.mobdeve.s11.pokeplan;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfileActivity extends AppCompatActivity {
     private SharedPreferences sp;
@@ -34,8 +28,6 @@ public class UserProfileActivity extends AppCompatActivity {
     private TextView tvEggHatchCtr;
     private TextView tvTaskCompleteCtr;
 
-    private Dialog confirmDialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,54 +38,55 @@ public class UserProfileActivity extends AppCompatActivity {
     }
 
     private void initComponents() {
-        this.btnback = findViewById(R.id.ib_userprofile_back);
-        this.btnback.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
-
         this.sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         this.spEditor = this.sp.edit();
+
+        this.btnback = findViewById(R.id.ib_userprofile_back);
         this.btnlogout = findViewById(R.id.btn_userprofile_logout);
-        this.btnlogout.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                spEditor.remove(Keys.KEY_EMAIL.name());
-                spEditor.remove(Keys.KEY_PASSWORD.name());
-                spEditor.apply();
-                UserSingleton.removeUser();
-                UserSingleton.getUser().logoutUser();
-                Intent intent = new Intent(UserProfileActivity.this, InitActivity.class);
-                startActivity(intent);
-            }
-        });
+        setButtonListeners();
 
         this.ivStarterIcon = findViewById(R.id.iv_userprofile_starter);
         this.tvUsername = findViewById(R.id.tv_userprofile_username);
         this.tvEmail = findViewById(R.id.tv_userprofile_email);
         this.tvName = findViewById(R.id.tv_userprofile_name);
-
         this.tvEggHatchCtr = findViewById(R.id.tv_userprofile_egghatchctr);
         this.tvTaskCompleteCtr = findViewById(R.id.tv_userprofile_taskcompletedctr);
+        setAllComponents();
     }
 
     private void setAllComponents() {
         UserDetails user = UserSingleton.getUser().getUserDetails();
 
+        // set user profile pic
         this.ivStarterIcon.setImageResource(getImageId(getApplicationContext(),
                 "pkmn_"+ user.getStarterDexNum()));
 
+        // set user details
         String username = "@" + user.getUserName();
         this.tvUsername.setText(username);
         this.tvEmail.setText(user.getEmail());
         this.tvName.setText(user.getFullName());
 
+        // set user stats
         String egg = user.getHatchedPkmnCount() + " Pokemon Hatched";
         this.tvEggHatchCtr.setText(egg);
-
         String task = user.getCompletedTaskCount() + " Tasks Completed";
         this.tvTaskCompleteCtr.setText(task);
+    }
 
+    private void setButtonListeners() {
+        this.btnback.setOnClickListener(view -> onBackPressed());
+        this.btnlogout.setOnClickListener(view -> logOutUser());
+    }
+
+    private void logOutUser() {
+        spEditor.remove(Keys.KEY_EMAIL.name());
+        spEditor.remove(Keys.KEY_PASSWORD.name());
+        spEditor.apply();
+        UserSingleton.removeUser();
+        UserSingleton.getUser().logoutUser();
+        Intent intent = new Intent(UserProfileActivity.this, InitActivity.class);
+        startActivity(intent);
     }
 
 
