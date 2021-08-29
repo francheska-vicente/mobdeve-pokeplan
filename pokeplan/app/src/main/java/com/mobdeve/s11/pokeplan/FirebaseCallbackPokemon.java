8cc76dfb42d5e38082670d5dfe4bad03fa6168ba
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-29 18:20:25 +0800

    Update FirebaseCallbackPokemon.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/FirebaseCallbackPokemon.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/FirebaseCallbackPokemon.java
@@ -5,5 +5,5 @@ import com.google.firebase.firestore.auth.User;
 import java.util.ArrayList;
 
 public interface FirebaseCallbackPokemon {
-    void onCallbackPokemon(ArrayList<UserPokemon> list);
+    void onCallbackPokemon(ArrayList<UserPokemon> list, Boolean isSuccessful, String message);
 }
