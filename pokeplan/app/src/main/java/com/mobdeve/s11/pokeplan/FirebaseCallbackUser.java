1476554967232e4de44d4f68450813aa4a609e13
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-08-29 18:20:31 +0800

    Update FirebaseCallbackUser.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/FirebaseCallbackUser.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/FirebaseCallbackUser.java
@@ -5,5 +5,5 @@ import com.google.firebase.firestore.auth.User;
 import java.util.ArrayList;
 
 public interface FirebaseCallbackUser {
-    void onCallbackUser(UserDetails userDetails);
+    void onCallbackUser(UserDetails userDetails, Boolean isSuccessful, String message);
 }
