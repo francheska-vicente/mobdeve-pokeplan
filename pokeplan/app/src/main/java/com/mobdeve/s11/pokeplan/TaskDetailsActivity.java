b8347b6f52bc05a569ae6a8269db2a54ab7db8b8
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-31 04:04:59 +0800

    Update TaskDetailsActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskDetailsActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/TaskDetailsActivity.java
@@ -57,10 +57,19 @@ public class TaskDetailsActivity extends AppCompatActivity {
 
         initInfo();
 
-
         ImageButton btnback = findViewById(R.id.ib_taskdetails_back);
         btnback.setOnClickListener(view -> onBackPressed());
+    }
 
+    private void initInfo() {
+        databaseHelper = new DatabaseHelper();
+        databaseHelper.getUserDetails((userDetails, isSuccessful, message) -> {
+            user = userDetails;
+            initComponents();
+        });
+    }
+
+    private void initComponents() {
         this.tvTaskName = findViewById(R.id.tv_taskdetails_name);
         this.tvCategory = findViewById(R.id.tv_taskdetails_category);
         this.ivCategory = findViewById(R.id.iv_taskdetails_category);
@@ -75,28 +84,41 @@ public class TaskDetailsActivity extends AppCompatActivity {
         this.ibEditTask = findViewById(R.id.ib_taskdetails_edit);
         this.tvNotif = findViewById(R.id.tv_taskdetails_notif);
 
-
-    }
-
-    private void initInfo() {
-        databaseHelper = new DatabaseHelper();
-        databaseHelper.getUserDetails((userDetails, isSuccessful, message) -> {
-            user = userDetails;
-            initComponents();
-        });
-    }
-
-    private void initComponents() {
         if (wasEdited)
             updateViewComponents(taskID);
         else
-            setInfo();
+            setViewComponents();
         setButtonListeners();
     }
 
     private void setButtonListeners() {
         this.btnFinishTask.setOnClickListener(v -> createConfirmFinishDialog(v, taskID));
         this.ibDeleteTask.setOnClickListener(v -> createDeleteTaskDialog(v, taskID));
+    }
+
+    private void setViewComponents () {
+        Intent intent = getIntent();
+
+        String taskName = intent.getStringExtra(Keys.KEY_TASKNAME.name());
+        String category = intent.getStringExtra(Keys.KEY_CATEGORY.name());
+        int priority = intent.getIntExtra(Keys.KEY_PRIORITY.name(), 1);
+        String startDate = intent.getStringExtra(Keys.KEY_START_DATE.name());
+        String endDate = intent.getStringExtra(Keys.KEY_DEADLINE.name());
+        String notes = intent.getStringExtra(Keys.KEY_NOTES.name());
+        String taskID = intent.getStringExtra(Keys.KEY_ID.name());
+        String cEndDate = intent.getStringExtra(Keys.KEY_C_END_DATE.name());
+        String cStartDate = intent.getStringExtra(Keys.KEY_C_START_DATE.name());
+        String cEndTime = intent.getStringExtra(Keys.KEY_C_END_TIME.name());
+        String cStartTime = intent.getStringExtra(Keys.KEY_C_START_TIME.name());
+        String notifWhen = intent.getStringExtra(Keys.KEY_NOTIF_WHEN.name());
+        Boolean notifOn = intent.getBooleanExtra(Keys.KEY_NOTIF_ON.name(), false);
+        Boolean notifStartTime = intent.getBooleanExtra(Keys.KEY_NOTIF_START_TIME.name(), false);
+
+        setViewComponentValues(taskName, category, startDate, endDate,
+                notes, priority, notifWhen, notifOn, notifStartTime);
+
+        this.ibEditTask.setOnClickListener(v -> editTaskDetails(taskName, category, priority, notes, taskID, cEndDate, cStartDate, cEndTime, cStartTime,
+                notifWhen, notifOn, notifStartTime));
     }
 
     private void updateViewComponents (String taskID) {
@@ -127,38 +149,12 @@ public class TaskDetailsActivity extends AppCompatActivity {
             String sEndTime = formatter.format(endDate.getHour()) + ":" + formatter.format(endDate.getMinute());
             String sStartTime = formatter.format(startDate.getHour()) + ":" + formatter.format(startDate.getMinute());
 
-            ibEditTask.setOnClickListener(v -> editIntent(taskName, category, priority, notes, taskID, sEndDate, sStartDate, sEndTime, sStartTime,
+            ibEditTask.setOnClickListener(v -> editTaskDetails(taskName, category, priority, notes, taskID, sEndDate, sStartDate, sEndTime, sStartTime,
                     notifWhen, notifOn, notifStartTime));
         });
     }
 
-    private void setInfo () {
-        Intent intent = getIntent();
-
-        String taskName = intent.getStringExtra(Keys.KEY_TASKNAME.name());
-        String category = intent.getStringExtra(Keys.KEY_CATEGORY.name());
-        int priority = intent.getIntExtra(Keys.KEY_PRIORITY.name(), 1);
-        String startDate = intent.getStringExtra(Keys.KEY_START_DATE.name());
-        String endDate = intent.getStringExtra(Keys.KEY_DEADLINE.name());
-        String notes = intent.getStringExtra(Keys.KEY_NOTES.name());
-        String taskID = intent.getStringExtra(Keys.KEY_ID.name());
-        String cEndDate = intent.getStringExtra(Keys.KEY_C_END_DATE.name());
-        String cStartDate = intent.getStringExtra(Keys.KEY_C_START_DATE.name());
-        String cEndTime = intent.getStringExtra(Keys.KEY_C_END_TIME.name());
-        String cStartTime = intent.getStringExtra(Keys.KEY_C_START_TIME.name());
-        String notifWhen = intent.getStringExtra(Keys.KEY_NOTIF_WHEN.name());
-        Boolean notifOn = intent.getBooleanExtra(Keys.KEY_NOTIF_ON.name(), false);
-        Boolean notifStartTime = intent.getBooleanExtra(Keys.KEY_NOTIF_START_TIME.name(), false);
-
-        setValues (taskName, category, startDate, endDate,
-                notes, priority, notifWhen, notifOn, notifStartTime);
-
-        this.ibEditTask.setOnClickListener(v -> editIntent(taskName, category, priority, notes, taskID, cEndDate, cStartDate, cEndTime, cStartTime,
-                notifWhen, notifOn, notifStartTime));
-    }
-
-
-    private void setValues (String taskName, String category, String startDate, String endDate,
+    private void setViewComponentValues (String taskName, String category, String startDate, String endDate,
                             String notes, int priority, String notifWhen, Boolean notifOn, Boolean notifStartTime) {
         this.tvTaskName.setText(taskName);
         this.tvCategory.setText(category);
@@ -191,7 +187,7 @@ public class TaskDetailsActivity extends AppCompatActivity {
         this.tvNotif.setText(notif);
     }
 
-    private void editIntent (String taskName, String category, int priority, String notes, String taskID,
+    private void editTaskDetails (String taskName, String category, int priority, String notes, String taskID,
                              String endDate, String startDate, String endTime, String startTime, String notifWhen,
                              Boolean notifOn, Boolean notifStartTime) {
         Intent intent = new Intent(TaskDetailsActivity.this, AddTaskActivity.class);
@@ -230,14 +226,67 @@ public class TaskDetailsActivity extends AppCompatActivity {
                     Boolean notifOn = intent.getBooleanExtra(Keys.KEY_NOTIF_ON.name(), false);
                     Boolean notifStartTime = intent.getBooleanExtra(Keys.KEY_NOTIF_START_TIME.name(), false);
 
-                    setValues(name, category, startDate, endDate,
+                    setViewComponentValues(name, category, startDate, endDate,
                             notes, priority, notifWhen, notifOn, notifStartTime);
                     wasEdited = true;
-                } else {
+                }
+                else {
                     initComponents();
                 }
             }
     );
+
+    private void giveCandies () {
+        /*
+            These five arrays represent the values needed to generate the candies
+            for each priority level
+
+            [0] - divider
+            [1] - max value for rare candy
+            [2] - min value for rare candy
+            [3] - max value for super candy
+            [4] - min value for super candy
+        */
+
+        int[] one = {90, 5, 3, 2, 1};
+        int[] two = {85, 10, 4, 3, 2};
+        int[] three = {80, 13, 5, 4, 3};
+        int[] four = {70, 17, 6, 5, 4};
+        int[] five = {60, 20, 7, 6, 7};
+
+        int priority = this.tvPriorityIcon.getText().toString().trim().length();
+
+        int[] candyrates;
+        switch (priority) {
+            case 1: candyrates = one; break;
+            case 2: candyrates = two; break;
+            case 3: candyrates = three; break;
+            case 4: candyrates = four; break;
+            default: candyrates = five; break;
+        }
+
+        HashMap<String, Object> hash = new HashMap<>();
+        int numberOfCandies;
+
+        int numberGenerated = new Random().nextInt(100) + 1;
+        if (numberGenerated > candyrates[0]) {
+            numberOfCandies = (int) (Math.random () *
+                    (candyrates[3] - candyrates[4] + 1) + candyrates[4]);
+            user.addSuperCandy(numberOfCandies);
+
+            hash.put("superCandy", user.getSuperCandy());
+            createGiveCandyDialog("superCandy", numberOfCandies);
+        }
+        else {
+            numberOfCandies = (int) (Math.random () *
+                    (candyrates[1] - candyrates[2] + 1) + candyrates[2]);
+            user.addRareCandy(numberOfCandies);
+            hash.put("rareCandy", user.getRareCandy());
+            createGiveCandyDialog("rareCandy", numberOfCandies);
+        }
+
+        databaseHelper.updateUser((userDetails, isSuccessful, message) -> {}, hash);
+    }
 
     private void createDeleteTaskDialog(View view, String taskID) {
         confirmDelete = new CustomDialog(view.getContext());
@@ -277,63 +326,7 @@ public class TaskDetailsActivity extends AppCompatActivity {
         confirmFinish.show();
     }
 
-    private void giveCandies () {
-        int divider = 90, maxR = 5, minR = 3, maxS = 2, minS = 1;
-        int priority = this.tvPriorityIcon.getText().toString().trim().length();
-
-        switch (priority) {
-            case 2: divider = 85;
-                maxR = 10;
-                minR = 4;
-                maxS = 3;
-                minS = 2;
-                break;
-            case 3: divider = 80;
-                maxR = 13;
-                minR = 5;
-                maxS = 4;
-                minS = 3;
-                break;
-            case 4: divider = 70;
-                maxR = 17;
-                minR = 6;
-                maxS = 5;
-                minS = 4;
-                break;
-            case 5: divider = 60;
-                maxR = 20;
-                minR = 7;
-                maxS = 6;
-                minS = 7;
-                break;
-        }
-
-        int numberGenerated = new Random().nextInt(100) + 1;
-        String candyType = "rareCandy";
-
-        int numberROfCandies = (int) (Math.random () * (maxR - minR + 1) + minR);
-        int numberSOfCandies = (int) (Math.random () * (maxS - minS + 1) + minS);
-
-        int totalNumberOfcandies,
-                numberOfCandies = numberROfCandies;
-        if (numberGenerated > divider) {
-            candyType = "superCandy";
-            numberOfCandies = numberSOfCandies;
-            user.addSuperCandy(numberSOfCandies);
-            totalNumberOfcandies = user.getSuperCandy();
-        } else {
-            user.addRareCandy(numberROfCandies);
-            totalNumberOfcandies = user.getRareCandy();
-        }
-
-        HashMap<String, Object> hash = new HashMap<>();
-        hash.put(candyType, totalNumberOfcandies);
-
-        databaseHelper.updateUser((userDetails, isSuccessful, message) -> {}, hash);
-        createCandyDialog(candyType, numberOfCandies);
-    }
-
-    private void createCandyDialog (String candyType, int numberOfCandies) {
+    private void createGiveCandyDialog (String candyType, int numberOfCandies) {
         candyDialog = new CustomDialog(TaskDetailsActivity.this);
         candyDialog.setDialogType(CustomDialog.OK);
 
