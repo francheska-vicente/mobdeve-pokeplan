4f20ca1d73beb1ff011a9dafc60ac22234dbb83e
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-08-31 00:20:51 +0800

    Update UserPokemon.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserPokemon.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/UserPokemon.java
@@ -1,148 +1,215 @@
 package com.mobdeve.s11.pokeplan;
 
-import com.google.firebase.database.Exclude;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
 public class UserPokemon {
+    private String userPokemonID;
 
     private Pokemon details;
     private String nickname;
     private String nature;
-
-    private Date dMetDate;
-
-    private boolean inParty;
-
     private int level;
     private int fedCandy;
+    private Date dMetDate;
+    private boolean inParty;
 
-    private String userPokemonID;
-
+    /**
+     * Class constructor (for database use)
+     */
     public UserPokemon() {}
 
-    public UserPokemon(Pokemon pokemon, String nickname, String nature, Date metDate,
-                        Boolean inParty, int level, int fedCandy, String pokemonID) {
-        this.details = pokemon;
-        this.nickname = nickname;
-        this.nature = nature;
-        this.dMetDate = metDate;
-        this.inParty = inParty;
-        this.level = level;
-        this.fedCandy = fedCandy;
-        this.userPokemonID = pokemonID;
-    }
-
+    /**
+     * Class constructor for generating a User's Pokemon
+     * @param details the species-specific details of a Pokemon
+     * @param inParty if true, the pokemon is currently in the user's party
+     *                else, the pokemon is in the PC
+     */
     public UserPokemon(Pokemon details, boolean inParty) {
         this.details = details;
+        this.inParty = inParty;
+
+        // default/initial nickname is the pokemon's species
         this.nickname = details.getSpecies();
+
+        // nature is randomly generated
         this.nature = this.generateNature();
-        this.dMetDate = new java.util.Date();
+
+        // pokemon level initialized to 1, candy initialized to 0
         this.level = 1;
         this.fedCandy = 0;
-        this.inParty = inParty;
+
+        // met date is time of UserPokemon object creation
+        this.dMetDate = new java.util.Date();
     }
 
+    /**
+     * Feeds candy to a pokemon. Every ten candies, the Pokemon's level will increase by 1
+     * until it reaches Level 100.
+     * @return  true, if pokemon leveled up
+     *          false, otherwise
+     */
     public boolean feedCandy() {
         fedCandy++;
-        if (fedCandy%10 == 0) {
+        if (fedCandy%10 == 0 && level < 100) {
             levelUpPokemon();
             return true;
         }
         return false;
     }
 
+    /**
+     * Increases a Pokemon's level by 1.
+     */
     public void levelUpPokemon() {
-        level++;
+        if (level < 100) level++;
     }
 
-    @Exclude
+    /**
+     * Evolves a Pokemon if it can still evolve and has met the required level.
+     */
     public void evolvePokemon() {
-        Pokedex pokedex = Pokedex.getPokedex();
-        if (nickname.equals(details.getSpecies()))
-            nickname = this.details.getEvolvesTo();
-        this.details = pokedex.getPokemon(this.details.getEvolvesTo());
+        if (!this.details.getEvolvesTo().isEmpty() && this.level >= this.details.getEvolveLvl()) {
+            if (nickname.equals(details.getSpecies()))
+                nickname = this.details.getEvolvesTo();
+            this.details = Pokedex.getPokedex().getPokemon(this.details.getEvolvesTo());
+        }
     }
 
-    @Exclude
-    public Pokemon getPokemonDetails() {
-        return this.details;
+    /**
+     * @return the ID of the UserPokemon
+     */
+    public String getUserPokemonID() {
+        return this.userPokemonID;
     }
 
+    /**
+     * @return the species-specific details of a UserPokemon
+     */
     public Pokemon getDetails () {
         return this.details;
     }
 
+    /**
+     * @return the nickname of the UserPokemon
+     */
     public String getNickname() {
         return this.nickname;
     }
 
+    /**
+     * @return the nature of the UserPokemon
+     */
     public String getNature() {
         return this.nature;
     }
 
+    /**
+     * @return the level of the UserPokemon
+     */
+    public int getLevel() {
+        return this.level;
+    }
+
+    /**
+     * @return the total candies fed to the UserPokemon
+     */
+    public int getFedCandy() {
+        return this.fedCandy;
+    }
+
+    /**
+     * @return the date when the UserPokemon was received in "MMMM dd, yyyy" format
+     */
     public String getMetDate() {
         SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd, yyyy");
         return formatter.format(dMetDate);
     }
 
+    /**
+     * @return the date when the UserPokemon was received (for database use)
+     */
     public Date getDMetDate () {
         return this.dMetDate;
     }
-    public String getUserPokemonID() {
-        return this.userPokemonID;
-    }
 
-    public int getLevel() {
-        return this.level;
-    }
-
-    public void setDMetDate (Date date) {
-        this.dMetDate = date;
-    }
-
-    public int getPercentToNextLevel() {
-        return fedCandy % 10 * 10;
-    }
-
-    public int getFedCandy() {
-        return this.fedCandy;
-    }
-
+    /**
+     * @return  true, if pokemon is currently in the party
+     *          false, if pokemon is in the PC
+     */
     public boolean isInParty () {
         return this.inParty;
     }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
+    /**
+     * @param id the ID of the UserPokemon (for database use)
+     */
     public void setUserPokemonID(String id) {
         this.userPokemonID = id;
     }
 
-    public void setLevel(int level) {
-        this.level = level;
+    /**
+     * @param nickname the nickname of the UserPokemon
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
+    /**
+     * @param pokemon the species-specific details of the UserPokemon (for database use)
+     */
     public void setDetails (Pokemon pokemon) {
         this.details = pokemon;
     }
 
-    public void setFedCandy(int fedcandy) {
-        this.fedCandy = fedcandy;
-    }
-
-    public void setInParty(boolean inParty) {
-        this.inParty = inParty;
-    }
-
+    /**
+     * @param nature the nature of the UserPokemon (for database use)
+     */
     public void setNature(String nature) {
         this.nature = nature;
     }
 
+    /**
+     * @param level the level of the UserPokemon (for database use)
+     */
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    /**
+     * @param fedcandy the total amount of candies fed to the UserPokemon (for database use)
+     */
+    public void setFedCandy(int fedcandy) {
+        this.fedCandy = fedcandy;
+    }
+
+    /**
+     * @param date the date when the UserPokemon was received (for database use)
+     */
+    public void setDMetDate (Date date) {
+        this.dMetDate = date;
+    }
+
+    /**
+     * @param inParty true, if pokemon is in the party (for database use)
+     *                false, if Pokmeon is in the PC
+     */
+    public void setInParty(boolean inParty) {
+        this.inParty = inParty;
+    }
+
+    /**
+     * @return the percentage of candies eaten relative to when the pokemon last leveled up
+     */
+    public int getPercentToNextLevel() {
+        return fedCandy % 10 * 10;
+    }
+
+    /**
+     * Generates a random nature from the list of natures
+     * @return the randomly generated nature
+     */
     public String generateNature () {
         String[] NATURES = {
                 "Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Bold", "Docile",
@@ -154,6 +221,10 @@ public class UserPokemon {
         return NATURES[new Random().nextInt(NATURES.length)];
     }
 
+    /**
+     * @return the string representation of the UserPokemon
+     */
+    @Override
     public String toString () {
         return this.userPokemonID;
     }
