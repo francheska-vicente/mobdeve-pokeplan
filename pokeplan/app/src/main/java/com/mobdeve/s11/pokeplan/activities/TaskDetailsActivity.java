d3d031f5162ba27366b01a113172f583fb143d7e
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-09-05 19:02:38 +0800

    Update TaskDetailsActivity.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/activities/TaskDetailsActivity.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/activities/TaskDetailsActivity.java
@@ -19,14 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.mobdeve.s11.pokeplan.R;
 import com.mobdeve.s11.pokeplan.data.DatabaseHelper;
-import com.mobdeve.s11.pokeplan.data.FirebaseCallbackTask;
 import com.mobdeve.s11.pokeplan.models.UserDetails;
-import com.mobdeve.s11.pokeplan.models.UserTask;
 import com.mobdeve.s11.pokeplan.utils.Keys;
 import com.mobdeve.s11.pokeplan.views.CustomDialog;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -130,8 +127,8 @@ public class TaskDetailsActivity extends AppCompatActivity {
         ImageButton btnback = findViewById(R.id.ib_taskdetails_back);
         btnback.setOnClickListener(view -> onBackPressed());
 
-        this.btnFinishTask.setOnClickListener(v -> createConfirmFinishDialog(v, taskID));
-        this.ibDeleteTask.setOnClickListener(v -> createDeleteTaskDialog(v, taskID));
+        this.btnFinishTask.setOnClickListener(v -> createConfirmFinishDialog());
+        this.ibDeleteTask.setOnClickListener(v -> createDeleteTaskDialog());
         this.ibEditTask.setOnClickListener(v -> editTaskDetails());
     }
 
@@ -265,6 +262,11 @@ public class TaskDetailsActivity extends AppCompatActivity {
         intent.putExtra(Keys.KEY_NOTIF_START_TIME.name(), notifStartTime);
     }
 
+    /**
+     * Converts time in 12HR format into 24HR format
+     * @param time the time in 12 hour format
+     * @return the time in 24 hour format
+     */
     private String convertTo24Hour (String time) {
         String [] temp = time.split(":");
         String meridian = temp[1].split(" ")[1];
@@ -316,12 +318,15 @@ public class TaskDetailsActivity extends AppCompatActivity {
             }
     );
 
+    /**
+     * Generates an amount of rare candies or super candies after finishing a task
+     */
     private void giveCandies() {
         /*
             These five arrays represent the values needed to generate the candies
             for each priority level
 
-            [0] - divider
+            [0] - divider (affects chances of getting rare or super candy)
             [1] - max value for rare candy
             [2] - min value for rare candy
             [3] - max value for super candy
@@ -334,8 +339,8 @@ public class TaskDetailsActivity extends AppCompatActivity {
         int[] four = {70, 17, 6, 5, 4};
         int[] five = {60, 20, 7, 6, 7};
 
+        // fix candy rates based on priority
         int priority = this.tvPriorityIcon.getText().toString().trim().length();
-
         int[] candyrates;
         switch (priority) {
             case 1: candyrates = one; break;
@@ -345,10 +350,12 @@ public class TaskDetailsActivity extends AppCompatActivity {
             default: candyrates = five; break;
         }
 
+        // generate candies and update user candy amount
         HashMap<String, Object> hash = new HashMap<>();
         int numberOfCandies;
-
         int numberGenerated = new Random().nextInt(100) + 1;
+
+        // candy generated was super candy
         if (numberGenerated > candyrates[0]) {
             numberOfCandies = (int) (Math.random () *
                     (candyrates[3] - candyrates[4] + 1) + candyrates[4]);
@@ -357,6 +364,8 @@ public class TaskDetailsActivity extends AppCompatActivity {
             hash.put("superCandy", user.getSuperCandy());
             createGiveCandyDialog("superCandy", numberOfCandies);
         }
+
+        // candy generated was rare candy
         else {
             numberOfCandies = (int) (Math.random () *
                     (candyrates[1] - candyrates[2] + 1) + candyrates[2]);
@@ -364,12 +373,14 @@ public class TaskDetailsActivity extends AppCompatActivity {
             hash.put("rareCandy", user.getRareCandy());
             createGiveCandyDialog("rareCandy", numberOfCandies);
         }
-
         databaseHelper.updateUser((userDetails, isSuccessful, message) -> {}, hash);
     }
 
-    private void createDeleteTaskDialog(View view, String taskID) {
-        confirmDelete = new CustomDialog(view.getContext());
+    /**
+     * Creates a confirm dialog for when the user wants to delete a task
+     */
+    private void createDeleteTaskDialog() {
+        confirmDelete = new CustomDialog(this);
         confirmDelete.setDialogType(CustomDialog.CONFIRM);
 
         confirmDelete.setConfirmComponents(
@@ -382,25 +393,25 @@ public class TaskDetailsActivity extends AppCompatActivity {
         Button btndialogconfirm = confirmDelete.findViewById(R.id.btn_dialog_confirm);
         btndialogconfirm.setOnClickListener(v -> {
             confirmDelete.dismiss();
-            databaseHelper.deleteTask(new FirebaseCallbackTask() {
-                @Override
-                public void onCallbackTask(ArrayList<UserTask> list, Boolean isSuccesful, String message) {
+            databaseHelper.deleteTask((list, isSuccesful, message) -> {
+                finish();
+                if (isSuccesful) {
+                    Toast.makeText(TaskDetailsActivity.this, "Task was successfully deleted.", Toast.LENGTH_SHORT).show();
                     finish();
-
-                    if (isSuccesful) {
-                        Toast.makeText(TaskDetailsActivity.this, "Task was successfully deleted.", Toast.LENGTH_SHORT).show();
-                        finish();
-                    } else {
-                        Toast.makeText(TaskDetailsActivity.this, "Task was not deleted.", Toast.LENGTH_SHORT).show();
-                    }
+                }
+                else {
+                    Toast.makeText(TaskDetailsActivity.this, "Task was not deleted.", Toast.LENGTH_SHORT).show();
                 }
             }, taskID);
         });
         confirmDelete.show();
     }
 
-    private void createConfirmFinishDialog(View view, String taskID) {
-        confirmFinish = new CustomDialog(view.getContext());
+    /**
+     * Creates a confirm dialog for when the user wants to finish a task
+     */
+    private void createConfirmFinishDialog() {
+        confirmFinish = new CustomDialog(this);
         confirmFinish.setDialogType(CustomDialog.CONFIRM);
 
         confirmFinish.setConfirmComponents(
@@ -418,6 +429,9 @@ public class TaskDetailsActivity extends AppCompatActivity {
         confirmFinish.show();
     }
 
+    /**
+     * Creates an OK dialog for when the user receives candy
+     */
     private void createGiveCandyDialog(String candyType, int numberOfCandies) {
         candyDialog = new CustomDialog(TaskDetailsActivity.this);
         candyDialog.setDialogType(CustomDialog.OK);
