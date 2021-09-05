c3432821b531af0ae7bf8fbd473e04916e0d1583
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-09-05 19:02:10 +0800

    Update FaqsViewHolder.java

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/viewholders/FaqsViewHolder.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/viewholders/FaqsViewHolder.java
@@ -11,10 +11,13 @@ import com.mobdeve.s11.pokeplan.R;
 import org.jetbrains.annotations.NotNull;
 
 public class FaqsViewHolder extends RecyclerView.ViewHolder {
-
     private TextView tvQuestion;
     private TextView tvAnswer;
 
+    /**
+     * Class constructor
+     * @param itemView the layout of a specific item
+     */
     public FaqsViewHolder(@NonNull @NotNull View itemView) {
         super(itemView);
 
@@ -22,10 +25,18 @@ public class FaqsViewHolder extends RecyclerView.ViewHolder {
         this.tvAnswer = itemView.findViewById(R.id.tv_faqs_template_answer);
     }
 
+    /**
+     * Sets the question in the TextView
+     * @param question the question to be set
+     */
     public void setTvQuestion (String question) {
         this.tvQuestion.setText(question);
     }
 
+    /**
+     * Sets the answer in the TextView
+     * @param answer the answer to be set
+     */
     public void setTvAnswer (String answer) {
         this.tvAnswer.setText(answer);
     }
