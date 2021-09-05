665fc114899026370c48531e95ed7e4fac0511d7
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-09-05 13:41:18 +0800

    Updated Custom Dialogs

--- pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/views/CustomDialog.java
+++ pokeplan/app/src/main/java/com/mobdeve/s11/pokeplan/views/CustomDialog.java
@@ -18,6 +18,7 @@ public class CustomDialog extends Dialog {
     public static final int CONFIRM = 2;
     public static final int ONE_INPUT = 3;
     public static final int TWO_INPUT = 4;
+    public static final int FOUR_INPUT = 5;
 
     private Context context;
 
@@ -42,6 +43,7 @@ public class CustomDialog extends Dialog {
             case CONFIRM: this.setContentView(R.layout.dialog_confirm); break;
             case ONE_INPUT: this.setContentView(R.layout.dialog_oneinput); break;
             case TWO_INPUT: this.setContentView(R.layout.dialog_twoinputs); break;
+            case FOUR_INPUT: this.setContentView(R.layout.dialog_fourinputs); break;
         }
     }
 
@@ -94,13 +96,65 @@ public class CustomDialog extends Dialog {
         this.setCancelable(false);
         this.setCanceledOnTouchOutside(false);
 
-        TextView tvdialogtitle = this.findViewById(R.id.iv_dialog_stringinput_title);
+        TextView tvdialogtitle = this.findViewById(R.id.iv_dialog_oneinput_title);
         tvdialogtitle.setText(title);
 
-        ImageView ivdialogicon = this.findViewById(R.id.iv_dialog_stringinput_icon);
+        ImageView ivdialogicon = this.findViewById(R.id.iv_oneinput_icon);
         ivdialogicon.setImageResource(icon);
 
-        Button btndialogcancel = this.findViewById(R.id.btn_dialog_stringinput_confirm);
+        Button btndialogcancel = this.findViewById(R.id.btn_dialog_oneinput_confirm);
+        btndialogcancel.setOnClickListener(v -> this.dismiss());
+    }
+
+    public void setTwoInputComponents(String title, String input1Title,
+                                      String input2Title, String confirmButtonText) {
+        this.setCancelable(false);
+        this.setCanceledOnTouchOutside(false);
+
+        TextView tvdialogtitle = this.findViewById(R.id.tv_dialog_confirm_title);
+        tvdialogtitle.setText(title);
+
+        TextView tvdialoginput1title = this.findViewById(R.id.tv_dialog_twoinputs_one_title);
+        tvdialoginput1title.setText(input1Title);
+
+        TextView tvdialoginput2title = this.findViewById(R.id.tv_dialog_twoinputs_two_title);
+        tvdialoginput2title.setText(input2Title);
+
+        Button btnDialogConfirm = this.findViewById(R.id.btn_dialog_twoinputs_confirm);
+        btnDialogConfirm.setText(confirmButtonText);
+
+        Button btndialogcancel = this.findViewById(R.id.btn_dialog_twoinputs_cancel);
+        btndialogcancel.setOnClickListener(v -> this.dismiss());
+    }
+
+    public void setFourInputComponents(String title,
+                                       String input1Title,
+                                       String input2Title,
+                                       String input3Title,
+                                       String input4Title,
+                                       String confirmButtonText) {
+        this.setCancelable(false);
+        this.setCanceledOnTouchOutside(false);
+
+        TextView tvdialogtitle = this.findViewById(R.id.tv_dialog_title);
+        tvdialogtitle.setText(title);
+
+        TextView tvdialoginput1title = this.findViewById(R.id.tv_dialog_fourinput_one_title);
+        tvdialoginput1title.setText(input1Title);
+
+        TextView tvdialoginput2title = this.findViewById(R.id.tv_dialog_fourinput_two_title);
+        tvdialoginput2title.setText(input2Title);
+
+        TextView tvdialoginput3title = this.findViewById(R.id.tv_dialog_fourinput_three_title);
+        tvdialoginput3title.setText(input3Title);
+
+        TextView tvdialoginput4title = this.findViewById(R.id.tv_dialog_fourinput_four_title);
+        tvdialoginput4title.setText(input4Title);
+
+        Button btnDialogConfirm = this.findViewById(R.id.btn_dialog_fourinput_confirm);
+        btnDialogConfirm.setText(confirmButtonText);
+
+        Button btndialogcancel = this.findViewById(R.id.btn_dialog_fourinput_cancel);
         btndialogcancel.setOnClickListener(v -> this.dismiss());
     }
 }
